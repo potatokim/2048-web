c3c242aacb64449fc075c7df5d43294adb32a4f4
Kim DAO <kdaokdao@gmail.com>
2020-09-17 16:47:36 -0700

    Updates

---
+++ precache-manifest.88a4708c1e1ad5d5b62ae6bf8dab17eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb4f6e65d849649be15fe1e697eefa7",
+    "url": "/2048-web/index.html"
+  },
+  {
+    "revision": "a8dbece3c7cebd7e613a",
+    "url": "/2048-web/static/css/main.cc922059.chunk.css"
+  },
+  {
+    "revision": "f3d3a8b7b3db61de1d39",
+    "url": "/2048-web/static/js/2.ec1ef6aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2048-web/static/js/2.ec1ef6aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8dbece3c7cebd7e613a",
+    "url": "/2048-web/static/js/main.3e61e385.chunk.js"
+  },
+  {
+    "revision": "06ff91ee31c97d351462",
+    "url": "/2048-web/static/js/runtime-main.0996ae2d.js"
+  }
+]);
